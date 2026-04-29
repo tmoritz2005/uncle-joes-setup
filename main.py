ee91956de0a74e14f54101f65b850e603bb5a25c
Theo Moritz <theodore.w.moritz@gmail.com>
2026-04-29 14:54:10 +0000

    COrs fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Uncle Joe's Coffee API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://uncle-joes-frontend-1055452553205.us-central1.run.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
